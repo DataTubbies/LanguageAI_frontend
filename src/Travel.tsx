0e8bce27d785cadd0f5b1cbd5aca209dccf85df8
Mads <mk@magnutron.dk>
2024-04-18 11:55:59 +0200

    lidt mer

--- src/Travel.tsx
+++ src/Travel.tsx
@@ -4,8 +4,8 @@ import "./App.css";
 
 const SERVER_URL = "http://localhost:9090/api/v1/";
 
-async function getJokeWithRateLimit(event) {
-  const URL = `${SERVER_URL}travellimited?about= + ${document.getElementById("about2").value}`;
+async function getTravelWithRateLimit(event) {
+  const URL = `${SERVER_URL}travel`;
   const requestBody = {
     method: "POST",
     headers: {
@@ -13,7 +13,7 @@ async function getJokeWithRateLimit(event) {
     },
     body: JSON.stringify({
       budget: document.getElementById("budget").value,
-      start: document.getElementById("start").value,
+      startingLocation: document.getElementById("start").value,
       destination: document.getElementById("destination").value,
       month: document.getElementById("month").value,
       duration: document.getElementById("duration").value,
@@ -44,7 +44,7 @@ export default function Travel() {
   const handleClick = (event) => {
     // Prevent the form from reloading the page.
     event.preventDefault();
-    getJokeWithRateLimit().then((res) => setAnswer(res));
+    getTravelWithRateLimit().then((res) => setAnswer(res));
   };
 
   return (
@@ -60,11 +60,16 @@ export default function Travel() {
             <input className="font-mono mt-1 w-1/3 text-center rounded-md border border-black-950 focus:ring-astronaut-blue-500 focus:border-astronaut-blue-500 drop-shadow-xl" type="number" id="budget" required />
           </div>
           <div>
-            <label htmlFor="start" className="font-mono">
+            <label htmlFor="startingLocation" className="font-mono">
               Where are you planning to travel from?
             </label>
             <br />
-            <input className="font-mono mt-1 w-1/3 text-center rounded-md border border-black-950 focus:ring-astronaut-blue-500 focus:border-astronaut-blue-500 drop-shadow-xl" type="text" id="start" required />
+            <input
+              className="font-mono mt-1 w-1/3 text-center rounded-md border border-black-950 focus:ring-astronaut-blue-500 focus:border-astronaut-blue-500 drop-shadow-xl"
+              type="text"
+              id="startingLocation"
+              required
+            />
           </div>
           <div>
             <label htmlFor="destination" className="font-mono">
