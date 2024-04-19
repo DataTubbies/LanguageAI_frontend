869e16d24b865f58fa411f2e2883c62b92a2b213
Nouhayla-code <nouhayla@hotmail.dk>
2024-04-19 15:30:10 +0200

    final css done

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Travel from "./Travel";
 
 function App() {
   return (
-    <div className="bg-[linear-gradient(to_bottom,rgba(49,84,44,0.0),rgba(16,71,52,0.8)),url('./assets/travel.jpg')] h-screen">
+    <div className="fixed bg-center bg-cover bg-[url('./assets/travel4.jpg')] h-screen">
       <Travel />
     </div>
   );
--- src/Travel.tsx
+++ src/Travel.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const SERVER_URL = "http://localhost:9090/api/v1/";
 
-async function handleHttpErrors(res) {
+async function handleHttpErrors(res: Response) {
   if (!res.ok) {
     const errorResponse = await res.json();
     const msg = errorResponse.message
@@ -24,9 +24,15 @@ type Travel = {
 
 export default function Travel() {
   const [answer, setAnswer] = useState(null);
-  const [err, setErr] = useState(null);
+  const [destination, setDestination] = useState("");
+  const [cityPhoto, setCityPhoto] = useState("");
+  const [transport, setTransport] = useState("");
+  const [activities, setActivities] = useState("");
+  const [accommodation, setAccommodation] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleClick = (event: React.FormEvent) => {
+    setLoading(true);
     event.preventDefault();
 
     const form = event.target as HTMLFormElement;
@@ -70,25 +76,29 @@ export default function Travel() {
       try {
         console.log(requestBody);
         const response = await fetch(URL, requestBody).then(handleHttpErrors);
+        console.log(response);
         setAnswer(response.answer);
-        document.getElementById("answerText2").innerHTML = response.answer;
+        setCityPhoto(response.cityPhoto);
+        setAccommodation(response.accommodation);
+        setActivities(response.activities);
+        setTransport(response.transport);
+        setDestination(response.destination);
+        setLoading(false);
       } catch (error) {
-        setErr(error);
+        console.log(error);
       }
     }
-
-    setErr(null);
-    console.log(err);
   };
 
   return (
-    <div className="grid grid-cols-2">
+    <div className="grid grid-cols-2 mx-40">
       <div className="grid md:grid-cols-1 mx-auto md:mx-20">
         <form
           className="space-y-3 w-full mt-5 text-center"
           id="form-joke2"
           onSubmit={handleClick}
         >
+          <br></br>
           <h1 className="font-mono text-2xl font-bold text-black-900">
             🤖Your AI travel helper🤖
           </h1>
@@ -172,13 +182,14 @@ export default function Travel() {
               required
             />
           </div>
+          <br />
           <div>
-            <div className="flex flex-wrap justify-center gap-6">
-              <button
-                type="submit"
-                className="font-mono relative inline-block h-full w-full rounded border-2 border-black bg-white px-3 py-1 text-base font-bold text-black transition duration-100 hover:bg-yellow-400 hover:text-gray-900"
-              >
-                Get Suggestions 🚀
+            <div className="flex flex-wrap justify-center gap-6  ">
+              <button type="submit" className="relative">
+                <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-black"></span>
+                <span className="fold-bold relative inline-block h-full w-full rounded border-2 border-black bg-white px-3 py-1 text-base font-bold text-black transition duration-100 hover:bg-gray-100 hover:text-gray-900">
+                  Get Suggestions 🚀
+                </span>
               </button>
             </div>
           </div>
@@ -188,16 +199,51 @@ export default function Travel() {
         <br></br>
         <br></br>
 
-        <p
-          id="answerText2"
-          className="font-mono text-sm font-bold text-black-900"
-        ></p>
-        {answer && (
-          <p
-            id="answerText"
-            className="font-mono text-sm font-bold text-black-900"
-          ></p>
+        {loading && (
+          <>
+            <img
+              className="ml-40 w-1/2 mt-56 grayscale brightness-200"
+              src="https://media4.giphy.com/media/U29iRRUrtx1wjD4GR4/giphy.gif?cid=6c09b952598ok0z57dia3o5j441rzncgrecw5bh1jiydaffh&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=s"
+            />
+            <p className="text-center font-mono text-2xl font-bold text-black-950">
+              LOADING...
+            </p>
+          </>
         )}
+
+        {answer && !loading ? (
+          <div className="ml-5">
+            <h2
+              id="answerDestination"
+              className="font-mono text-2xl font-bold text-black-950"
+            >
+              {destination}
+            </h2>
+            <br />
+            <img className="h-40" src={cityPhoto}></img>
+            <br />
+            <p
+              id="answerTransport"
+              className="font-mono text-sm text-black-950"
+            >
+              {transport}
+            </p>
+            <br />
+            <p
+              id="answerAccommodation"
+              className="font-mono text-sm text-black-950"
+            >
+              {accommodation}
+            </p>
+            <br />
+            <p
+              id="answerActivities"
+              className="font-mono text-sm text-black-950"
+            >
+              {activities}
+            </p>
+          </div>
+        ) : null}
       </div>
     </div>
   );
